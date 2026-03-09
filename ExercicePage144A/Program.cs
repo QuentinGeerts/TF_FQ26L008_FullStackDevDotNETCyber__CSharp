07a6c1f0d802214f31b542a51a9282c9e9ad7b44
Quentin Geerts <geerts.quentin@gmail.com>
2026-03-09 15:45:25 +0100

    Correcstion exercice page 144 - Fibonacci

---
+++ ExercicePage144A/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Calculer les 25 premiers nombres de la suite de Fibonacci
+ */
+
+int a = 0, b = 1, c;
+
+Console.WriteLine($"1. {a}");
+Console.WriteLine($"2. {b}");
+
+for (int i = 3; i <= 25; i++)
+{
+    c = a + b;
+    a = b;
+    b = c;
+    Console.WriteLine($"{i}. {c}");
+}
