2dca8f73b0628b8bd8b80168f0cef3f6f501f5ee
Quentin Geerts <geerts.quentin@gmail.com>
2026-03-09 16:27:35 +0100

    Correction exercice page 144 B

---
+++ ExercicePage144B/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Calculer le factoriel d’un nombre entré au clavier.
+ */
+
+int resultat = 1;
+int nombre;
+
+Console.WriteLine($"Entrez un nombre:");
+while (!int.TryParse(Console.ReadLine(), out nombre))
+    Console.WriteLine($"Erreur, réessayez :");
+
+
+for (int i = nombre; i >= 2; i--)
+{
+    //resultat = resultat * i;
+    resultat *= i;
+}
+
+Console.WriteLine($"{nombre}! = {resultat}");
