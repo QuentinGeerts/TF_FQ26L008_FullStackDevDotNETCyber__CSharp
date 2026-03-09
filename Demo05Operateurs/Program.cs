8db5c5c20b6d83caeadd6df07ec923a4707fb9be
Quentin Geerts <geerts.quentin@gmail.com>
2026-03-09 11:27:29 +0100

    Démonstration 05 - Opérateurs (Partie 2)

--- Demo05Operateurs/Program.cs
+++ Demo05Operateurs/Program.cs
@@ -61,7 +61,95 @@ result ??= "Valeur par défaut"; // Permet d'initialiser une valeur par défaut
 // 6. Opérateur typeof
 // typeof(type)
 
+// typeof       : Permet de récupérer le type d'un type spécifié
+// GetType()    : Permet de récupérer le type d'une instance
+
 Type stringType = typeof(string); // String
 Type resultType = result.GetType(); // String
 
 Console.WriteLine($"Result {(stringType == resultType ? "est" : "n'est pas")} de type string");
+
+// 7. Opérateur is
+// instance is Type
+
+// Permet de vérifier si une instance est d'un certains type
+
+object o = "BonJour";
+
+Console.WriteLine($"o.GetType(): {o.GetType()}");
+
+if (o is int)
+{
+    Console.WriteLine($"La variable 'o' est un entier.");
+}
+else if (o is short)
+{
+    Console.WriteLine($"La variable 'o' est un short.");
+}
+else if (o is long)
+{
+    Console.WriteLine($"La variable 'o' est un long.");
+}
+else if (o is string str) // Pattern matching
+{
+    Console.WriteLine($"La variable 'o' est une string.");
+    Console.WriteLine(((string)o).ToUpper());
+    Console.WriteLine(((string)o).ToLower());
+
+    Console.WriteLine(str.ToLower());
+    Console.WriteLine(str.ToUpper());
+}
+
+// Autre version du pattern matching (avec switch ♥)
+switch (o)
+{
+    case int:
+        Console.WriteLine($"o est un entier.");
+        break;
+
+    case string str:
+        Console.WriteLine(str.ToLower());
+        break;
+
+    default:
+        Console.WriteLine($"Type non pris en charge");
+        break;
+}
+
+
+// 8. Opérateur as
+// expression as type (type nullable)
+
+object myObject = "hello";
+
+string? myString = myObject as string; // myObject is string ? (string)myObject : null;
+
+object myObject2 = 42;
+
+int? myInt = myObject2 as int?; // myObject2 is int ? (int)myObject2 : null
+
+
+// 9. Opérateur checked / unchecked
+
+// Pour appliquer de manière automatique le checked sur le projet :
+// a. Clic droit sur le projet
+// b. Propriétés
+// c. Build
+// d. Advanced
+// e. Cocher la case "Check for arithmetic operations overflows"
+
+try
+{
+    checked
+    {
+        int x = int.MaxValue;
+        int y = x + 1;
+        Console.WriteLine($"x: {x}");
+        Console.WriteLine($"y: {y}");
+    }
+}
+catch (Exception)
+{
+
+    Console.WriteLine($"Exception levée");
+}
