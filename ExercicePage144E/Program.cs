4c152a426ff37012065337a2d67fbed7682a26ae
Quentin Geerts <geerts.quentin@gmail.com>
2026-03-10 10:13:10 +0100

    Correction de l'exercice page 144 E

---
+++ ExercicePage144E/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * À l’aide d’une boucle « for » comptez de 0, à 20,0 en augmentant de 0,1,
+ * en utilisant des doubles, et afficher la valeur à chaque itération.
+ * Remarquez-vous quelque chose de particulier ?
+ */
+
+for (double i = 0.0; i < 20.0; i += 0.1)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (decimal i = 0.0M; i <= 20.0M; i += 0.1M)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (double i = 0.0; i <= 200; i += 1)
+{
+    Console.WriteLine($"{i/10.0:F1}");
+}
