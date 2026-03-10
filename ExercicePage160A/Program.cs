b406b74537ce7504ef4729cee4491a64ff46b13a
Quentin Geerts <geerts.quentin@gmail.com>
2026-03-10 16:33:04 +0100

    Correction exercice page 160 A

---
+++ ExercicePage160A/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Grâce à une boucle « while » et à l’aide d’une collection, 
+ * calculez les nombres premiers inférieur à un nombre entier entré au clavier.
+ */
+
+using System.Collections;
+
+int limit;
+ArrayList primes = new ArrayList();
+
+Console.WriteLine($"Entrez le nombre maximum pour déterminer les nombres premiers :");
+while (!int.TryParse(Console.ReadLine(), out limit) || limit < 0)
+{
+    Console.WriteLine("Valeur incorrecte, réessayez :");
+}
+
+for (int i = 2; i <= limit; i++)
+{
+    bool isPrime = true;
+
+    for (int j = 2; j <= Math.Sqrt(i); j++)
+    {
+        if (i % j == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime)
+    {
+        primes.Add(i); 
+    }
+}
+
+
+foreach (var number in primes)
+{
+    Console.Write($"\t{number}");
+}
