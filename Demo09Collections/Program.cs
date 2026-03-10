92af3f6922951f5df3e80be05f0a0ec4dbe88f41
Quentin Geerts <geerts.quentin@gmail.com>
2026-03-10 14:40:50 +0100

    Démonstration 09 et 10 sur les collections et génériques

---
+++ Demo09Collections/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+
+/*
+ * Démonstration 09 - Les collections et collections génériques
+ */
+
+// 4 collections de base les plus utilisées
+// - ArrayList  →   List<T>
+// - Hashtable  →   Dictionary<T,U>
+// - Queue      →   Queue<T>
+// - Stack      →   Stack<T>
+
+// Pour utiliser une collection de base, il faut importer le namespace:
+// > System.Collections
+
+// 1.  ArrayList
+// Liste d'éléments
+
+ArrayList myArrayList = new ArrayList();
+
+myArrayList.Add("hello");
+myArrayList.Add("bonjour");
+myArrayList.Add("halo");
+myArrayList.Add("Hola");
+
+foreach (string greeting in myArrayList)
+{
+    Console.WriteLine($"greeting: {greeting}");
+}
+
+myArrayList.RemoveAt(1);
+
+Console.WriteLine();
+
+// 2. Hashtable
+// Clef/valeur
+
+Hashtable myHashtable = new Hashtable();
+
+myHashtable.Add("quentin.geerts@bstorm.be", "Quentin Geerts");
+myHashtable.Add("samuel.legrain@cognitic.be", "Samuel Legrain");
+myHashtable.Add("thierry.morre@cognitic.be", "Thierry Morre");
+
+if (!myHashtable.ContainsKey("quentin.geerts@bstorm.be"))
+    myHashtable.Add("quentin.geerts@bstorm.be", "Hello");
+else
+{
+    myHashtable["quentin.geerts@bstorm.be"] = "Hello";
+}
+
+foreach (DictionaryEntry contact in myHashtable)
+{
+    Console.WriteLine($"Contact: {contact.Key} {contact.Value}");
+}
+
+// 3.  Collection Queue
+// > FIFO = First In First Out
+
+//Queue fileAttente = new Queue();
+
+//fileAttente.Enqueue("Personne 1");
+//fileAttente.Enqueue("Personne 2");
+//fileAttente.Enqueue("Personne 3");
+//fileAttente.Enqueue("Personne 4");
+//fileAttente.Enqueue("Personne 5");
+
+//while(fileAttente.Count > 0)
+//{
+//    var personne = fileAttente.Peek();
+//    Console.WriteLine($"Prochaine personne: {personne}");
+
+//    Console.WriteLine($"Traitement en cours....");
+
+//    if (Random.Shared.Next(100) >= 70)
+//    {
+//        Console.WriteLine("Personne traitée avec succès");
+//        fileAttente.Dequeue();
+//    }
+//}
+
+
+//// 4.  Collection Stack
+//// Last In First Out (LIFO)
+
+//Stack pileAssiettes = new Stack();
+
+//pileAssiettes.Push("Assiette 1");
+//pileAssiettes.Push("Assiette 2");
+//pileAssiettes.Push("Assiette 3");
+//pileAssiettes.Push("Assiette 4");
+//pileAssiettes.Push("Assiette 5");
+
+//foreach (var assiette in pileAssiettes)
+//{
+//    Console.WriteLine($"Assiette: {assiette}");
+//}
+
+//while (pileAssiettes.Count > 0)
+//{
+//    var assiette = pileAssiettes.Peek();
+//    Console.WriteLine($"Assiette en cours de lavage: {assiette}");
+
+
+//    Console.WriteLine($"Lavée avec succès: {pileAssiettes.Pop()}");
+//}
---
+++ Demo10CollectionsGeneriques/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Démonstration 10 - Collections génériques
+ */
+
+// 4 collections de base les plus utilisées
+// - ArrayList  →   List<T>
+// - Hashtable  →   Dictionary<T,U>
+// - Queue      →   Queue<T>
+// - Stack      →   Stack<T>
+
+// 1.  Collection List<T>
+
+List<int> myIntList = new List<int>();
+
+List<int> myIntList3 = new List<int> { 1, 2, 3 };
+
+List<int> myIntList2 = [12, 13];
+
+
+
+myIntList.Add(42);
+//myIntList.Add("Hello"); // Ne fonctionne pas
+
+// 2. Dictionary<T,U>
+
+Dictionary<string, string> contacts = new Dictionary<string, string>();
+
+contacts.Add("quentin.geerts@bstorm.be", "Quentin Geerts");
+
+foreach (KeyValuePair<string, string> kvp in contacts)
+{
+    Console.WriteLine($"Contact: {kvp.Key} {kvp.Value}");
+}
+
+foreach (string email in contacts.Keys)
+{
+    Console.WriteLine($"Contact: Email: {email} | info: {contacts[email]}");
+}
+
+// 3. Queue<T> & Stack<T>
+
+Queue<string> myQueue = new Queue<string>();
+myQueue.Enqueue("Personne1");
+//myQueue.Enqueue(1);
+
+Stack<string> myStack = new Stack<string>();
+myStack.Push("");
+//myStack.Push(1);
