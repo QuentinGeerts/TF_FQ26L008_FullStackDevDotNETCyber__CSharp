6170efd9c67227b6d339a98ed9783a912ec3b977
Quentin Geerts <geerts.quentin@gmail.com>
2026-03-10 10:01:40 +0100

    Correction de l'exercice page 144 D

---
+++ ExercicePage144D/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * A l’aide de boucles « for » afficher les 5 premières tables de multiplication 
+ * en allant jusque « x20 ».
+ */
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"Table de {i} :");
+    for (int multiplicateur = 1; multiplicateur <= 20; multiplicateur++)
+    {
+        Console.WriteLine($"{multiplicateur} x {i} = {multiplicateur * i}");
+    }
+    Console.WriteLine();
+}
